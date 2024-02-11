34c0de7c834de3a4acaeb629a51d21a8009c29e1
Francesco Rivola <francesco@connectif.es>
2024-02-11 19:36:53 +0100

    WIP - lib

---
+++ src/fetch-with-retry.ts
@@ -0,0 +1,185 @@
+import fetch, { RequestInit, Response } from "node-fetch";
+
+export type OnRetry = {
+  response?: Response;
+  error?: any;
+  attempt: number;
+  delay: number;
+  rateLimitRetry: boolean;
+};
+
+export type FetchWithRetries = (
+  url: string,
+  requestInit: RequestInit,
+  options: {
+    onRetry?: (params: OnRetry) => void;
+  }
+) => Promise<Response>;
+
+/**
+ * A lib that provides an easy way to retry http requests due to errors and rate limits.
+ * It also provides an abort system to allow graceful shutdown when waiting for long retries.
+ */
+export function buildFetchWithRetries(options: {
+  maxRetries: number;
+  initialDelay: number;
+  factor: number;
+  rateLimit: {
+    maxRetries: number;
+    maxDelay: number;
+  };
+}): FetchWithRetries {
+  const { maxRetries, initialDelay, factor, rateLimit } = options;
+
+  /**
+   * @param url fetch url
+   * @param requestInit fetch request options
+   * @param options retry options
+   * @returns promise of a response
+   */
+  async function fetchWithRetries(
+    url: string,
+    requestInit: RequestInit,
+    options: {
+      onRetry?: (params: OnRetry) => void;
+    } = {}
+  ): Promise<Response> {
+    const { onRetry } = options;
+    const signal = requestInit.signal as AbortSignal;
+    let attempt = 0;
+    let errorRetries = 0;
+    let rateLimitRetries = 0;
+    let retry = false;
+    let rateLimitRetry = false;
+    let response: Response;
+    let error = null;
+
+    let aborted = false;
+    function setAborted() {
+      aborted = true;
+    }
+    signal?.addEventListener("abort", setAborted);
+
+    do {
+      response = null;
+      error = null;
+      attempt++;
+
+      try {
+        response = await fetch(url, requestInit);
+      } catch (e) {
+        if (e.type === "aborted") {
+          // do nothing
+        } else if (!hasReachedMaxRetries(errorRetries)) {
+          error = e;
+        } else {
+          signal?.removeEventListener("abort", setAborted);
+          throw e;
+        }
+      }
+
+      rateLimitRetry =
+        response &&
+        isRateLimitRetry(response) &&
+        !hasReachedRateLimitMaxRetries(rateLimitRetries);
+      retry =
+        error ||
+        (response &&
+          isErrorThatHaveToBeRetried(response) &&
+          !hasReachedMaxRetries(errorRetries)) ||
+        rateLimitRetry;
+
+      if (retry && !aborted) {
+        let delay: number;
+        if (rateLimitRetry) {
+          rateLimitRetries++;
+          delay = getRateLimitDelay(response);
+        } else {
+          errorRetries++;
+          delay = getDelay(errorRetries);
+        }
+        if (typeof onRetry === "function") {
+          onRetry({
+            error,
+            response,
+            attempt,
+            delay,
+            rateLimitRetry,
+          });
+        }
+        await wait(delay, signal);
+      }
+    } while (retry && !aborted);
+
+    signal?.removeEventListener("abort", setAborted);
+    if (aborted) {
+      signal?.throwIfAborted();
+    }
+
+    return response;
+  }
+
+  return fetchWithRetries;
+
+  function hasReachedMaxRetries(retries: number): boolean {
+    return retries >= maxRetries;
+  }
+
+  function hasReachedRateLimitMaxRetries(retries: number): boolean {
+    return retries >= rateLimit.maxRetries;
+  }
+
+  function getRateLimitDelay(response: Response): number {
+    const retryAfterDelay = getRetryAfterInMilliseconds(response);
+    return retryAfterDelay < rateLimit.maxDelay
+      ? retryAfterDelay
+      : rateLimit.maxDelay;
+  }
+
+  function getDelay(retries: number): number {
+    return initialDelay * Math.pow(factor, retries);
+  }
+}
+
+function isRateLimitRetry(response: Response): boolean {
+  // We check for 429 and 503 Retry-After header value if set
+  // see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+  return (
+    [429, 503].includes(response.status) &&
+    Number.isInteger(getRetryAfterInSeconds(response))
+  );
+}
+
+function getRetryAfterInSeconds(response: Response): number {
+  return parseInt(response.headers.get("Retry-After"), 10);
+}
+
+function getRetryAfterInMilliseconds(response: Response): number {
+  return getRetryAfterInSeconds(response) * 1000;
+}
+
+function isErrorThatHaveToBeRetried(response: Response): boolean {
+  return [408, 425, 429, 500, 502, 503, 504].includes(response.status);
+}
+
+function wait(
+  durationInMilliseconds: number,
+  signal?: AbortSignal
+): Promise<void> {
+  return new Promise<void>((resolve) => {
+    signal?.addEventListener("abort", handleAbort);
+
+    const internalTimer = setTimeout(internalResolve, durationInMilliseconds);
+
+    function internalResolve() {
+      signal?.removeEventListener("abort", handleAbort);
+      resolve();
+    }
+
+    function handleAbort() {
+      signal?.removeEventListener("abort", handleAbort);
+      clearTimeout(internalTimer);
+      resolve();
+    }
+  });
+}
---
+++ test/fetch-with-retry.spec.ts
@@ -0,0 +1,392 @@
+import nock from "nock";
+import { describe, beforeEach, afterEach, test } from "node:test";
+import { toEqual } from "node:assert";
+import { buildFetchWithRetries } from "../src/fetch-with-retry";
+
+const retryStatusCodes = [408, 425, 429, 500, 502, 503, 504];
+
+describe("fetch-with-retry", async () => {
+  await beforeEach(() => {
+    nock.disableNetConnect();
+  });
+
+  await afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  await test("should return the response if ok", async () => {
+    const nockScope = nock("https://test.com")
+      .get("/test")
+      .reply(200, { message: "ok" });
+    const fetchWithRetries = buildFetchWithRetries({
+      maxRetries: 1,
+      initialDelay: 1000,
+      factor: 2,
+      rateLimit: {
+        maxRetries: 10,
+        maxDelay: 60_000,
+      },
+    });
+    let retries = 0;
+
+    const response = await fetchWithRetries(
+      "https://test.com/test",
+      {
+        method: "GET",
+      },
+      {
+        onRetry: () => {
+          retries++;
+        },
+      }
+    );
+
+    toEqual(retries, 0, "retries");
+    expect(response.ok).to.equal(true);
+    const body = await response.json();
+    expect(body).to.be.deep.equal({ message: "ok" });
+    expect(nockScope.isDone()).to.equal(true);
+  });
+
+  await test("should return the response if not ok and cannot be retried", async () => {
+    const nockScope = nock("https://test.com")
+      .get("/test")
+      .reply(400, { message: "bad request" });
+    const fetchWithRetries = buildFetchWithRetries({
+      maxRetries: 1,
+      initialDelay: 1000,
+      factor: 2,
+      rateLimit: {
+        maxRetries: 10,
+        maxDelay: 60_000,
+      },
+    });
+    let retries = 0;
+
+    const response = await fetchWithRetries(
+      "https://test.com/test",
+      {
+        method: "GET",
+      },
+      {
+        onRetry: () => {
+          retries++;
+        },
+      }
+    );
+
+    expect(retries).to.equal(0, "retries");
+    expect(response.ok).to.equal(false);
+    const body = await response.json();
+    expect(body).to.be.deep.equal({ message: "bad request" });
+    expect(nockScope.isDone()).to.equal(true);
+  });
+
+  for (const status of retryStatusCodes) {
+    test(`should return the response if ok after retrying 3 times ${status} http status code response`, async () => {
+      const nockScope = nock("https://test.com")
+        .get("/test")
+        .times(3)
+        .reply(status, { message: "error" })
+        .get("/test")
+        .reply(200, { message: "ok" });
+      const fetchWithRetries = buildFetchWithRetries({
+        maxRetries: 3,
+        initialDelay: 0,
+        factor: 2,
+        rateLimit: {
+          maxRetries: 10,
+          maxDelay: 60_000,
+        },
+      });
+      let retries = 0;
+      let attempts = 0;
+
+      const response = await fetchWithRetries(
+        "https://test.com/test",
+        {
+          method: "GET",
+        },
+        {
+          onRetry: (params) => {
+            attempts = params.attempt;
+            retries++;
+          },
+        }
+      );
+
+      expect(retries).to.equal(3, "retries");
+      expect(attempts).to.equal(3, "attempts");
+      expect(response.ok).to.equal(true);
+      const body = await response.json();
+      expect(body).to.be.deep.equal({ message: "ok" });
+      expect(nockScope.isDone()).to.equal(true);
+    });
+  }
+
+  for (const status of retryStatusCodes) {
+    await test(`should return the response after retrying ${status} http status code response`, async () => {
+      const nockScope = nock("https://test.com")
+        .get("/test")
+        .times(4)
+        .reply(status, { message: "error" });
+      const fetchWithRetries = buildFetchWithRetries({
+        maxRetries: 3,
+        initialDelay: 0,
+        factor: 2,
+        rateLimit: {
+          maxRetries: 10,
+          maxDelay: 60_000,
+        },
+      });
+      let lastRetryIsRateLimitRetry;
+      let retries = 0;
+      let attempts = 0;
+
+      const response = await fetchWithRetries(
+        "https://test.com/test",
+        {
+          method: "GET",
+        },
+        {
+          onRetry: (params) => {
+            attempts = params.attempt;
+            lastRetryIsRateLimitRetry = params.rateLimitRetry;
+            retries++;
+          },
+        }
+      );
+
+      expect(retries).to.equal(3, "retries");
+      expect(attempts).to.equal(3, "attempts");
+      expect(lastRetryIsRateLimitRetry).to.equal(
+        false,
+        "last retry is rate limit retry"
+      );
+      expect(response.ok).to.equal(false);
+      expect(response.status).to.equal(status);
+      const body = await response.json();
+      expect(body).to.be.deep.equal({ message: "error" });
+      expect(nockScope.isDone()).to.equal(true);
+    });
+  }
+
+  for (const status of [503, 429]) {
+    await test(`should return the response after retrying 10 times ${status} http status code response`, async () => {
+      const nockScope = nock("https://test.com")
+        .get("/test")
+        .times(10)
+        .reply(status, { message: "error" }, { "Retry-After": "0" })
+        .get("/test")
+        .reply(200, { message: "ok" });
+      const fetchWithRetries = buildFetchWithRetries({
+        maxRetries: 3,
+        initialDelay: 0,
+        factor: 2,
+        rateLimit: {
+          maxRetries: 10,
+          maxDelay: 60_000,
+        },
+      });
+      let lastRetryIsRateLimitRetry;
+      let retries = 0;
+      let attempts = 0;
+
+      const response = await fetchWithRetries(
+        "https://test.com/test",
+        {
+          method: "GET",
+        },
+        {
+          onRetry: (params) => {
+            attempts = params.attempt;
+            lastRetryIsRateLimitRetry = params.rateLimitRetry;
+            retries++;
+          },
+        }
+      );
+
+      expect(retries).to.equal(10, "retries");
+      expect(attempts).to.equal(10, "attempts");
+      expect(lastRetryIsRateLimitRetry).to.equal(
+        true,
+        "last retry is rate limit retry"
+      );
+      expect(response.ok).to.equal(true);
+      expect(response.status).to.equal(200);
+      const body = await response.json();
+      expect(body).to.be.deep.equal({ message: "ok" });
+      expect(nockScope.isDone()).to.equal(true);
+    });
+  }
+
+  await test("should return the response if ok after retrying 3 times network errors", async () => {
+    const nockScope = nock("https://test.com")
+      .get("/test")
+      .times(3)
+      .replyWithError(new Error("Network error"))
+      .get("/test")
+      .reply(200, { message: "ok" });
+    const fetchWithRetries = buildFetchWithRetries({
+      maxRetries: 3,
+      initialDelay: 0,
+      factor: 2,
+      rateLimit: {
+        maxRetries: 10,
+        maxDelay: 60_000,
+      },
+    });
+    let retries = 0;
+    let attempts = 0;
+
+    const response = await fetchWithRetries(
+      "https://test.com/test",
+      {
+        method: "GET",
+      },
+      {
+        onRetry: (params) => {
+          attempts = params.attempt;
+          retries++;
+        },
+      }
+    );
+
+    expect(retries).to.equal(3, "retries");
+    expect(attempts).to.equal(3, "attempts");
+    expect(response.ok).to.equal(true);
+    const body = await response.json();
+    expect(body).to.be.deep.equal({ message: "ok" });
+    expect(nockScope.isDone()).to.equal(true);
+  });
+
+  await test("should throw the error after retrying network errors", async () => {
+    const nockScope = nock("https://test.com")
+      .get("/test")
+      .times(4)
+      .replyWithError(new Error("Network error"));
+    const fetchWithRetries = buildFetchWithRetries({
+      maxRetries: 3,
+      initialDelay: 0,
+      factor: 2,
+      rateLimit: {
+        maxRetries: 10,
+        maxDelay: 60_000,
+      },
+    });
+    let retries = 0;
+    let attempts = 0;
+    let error = null;
+
+    try {
+      await fetchWithRetries(
+        "https://test.com/test",
+        {
+          method: "GET",
+        },
+        {
+          onRetry: (params) => {
+            attempts = params.attempt;
+            retries++;
+          },
+        }
+      );
+    } catch (e) {
+      error = e;
+    }
+
+    expect(retries).to.equal(3, "retries");
+    expect(attempts).to.equal(3, "attempts");
+    expect(error instanceof Error).to.equal(true, "error instance of error");
+    expect(error.message).to.equal(
+      "request to https://test.com/test failed, reason: Network error"
+    );
+    expect(nockScope.isDone()).to.equal(true);
+  });
+
+  await test("should abort while waiting if signal notify an abort", async () => {
+    const nockScope = nock("https://test.com")
+      .get("/test")
+      .times(4)
+      .replyWithError(new Error("Network error"));
+    const fetchWithRetries = buildFetchWithRetries({
+      maxRetries: 1,
+      initialDelay: 10000,
+      factor: 2,
+      rateLimit: {
+        maxRetries: 10,
+        maxDelay: 60_000,
+      },
+    });
+    let retries = 0;
+    let error = null;
+    const controller = new AbortController();
+    const abortError = new Error("Boom, is aborted");
+    try {
+      setTimeout(() => controller.abort(abortError), 50);
+      await fetchWithRetries(
+        "https://test.com/test",
+        {
+          method: "GET",
+          signal: controller.signal,
+        },
+        {
+          onRetry: () => {
+            retries++;
+          },
+        }
+      );
+    } catch (e) {
+      error = e;
+    }
+
+    expect(retries).to.equal(1, "retries");
+    expect(error instanceof Error).to.equal(true, "error instance");
+    expect(error.message).to.equal("Boom, is aborted");
+    expect(nockScope.isDone()).to.equal(false);
+  });
+
+  await test("should abort while making the request if signal notify an abort", async () => {
+    const nockScope = nock("https://test.com")
+      .get("/test")
+      .delay(200)
+      .reply(200, { success: true });
+    const fetchWithRetries = buildFetchWithRetries({
+      maxRetries: 1,
+      initialDelay: 10000,
+      factor: 2,
+      rateLimit: {
+        maxRetries: 10,
+        maxDelay: 60_000,
+      },
+    });
+    let retries = 0;
+    let error = null;
+    const controller = new AbortController();
+    const abortError = new Error("Boom, is aborted");
+
+    try {
+      setTimeout(() => controller.abort(abortError), 0);
+      await fetchWithRetries(
+        "https://test.com/test",
+        {
+          method: "GET",
+          signal: controller.signal,
+        },
+        {
+          onRetry: () => {
+            retries++;
+          },
+        }
+      );
+    } catch (e) {
+      error = e;
+    }
+
+    expect(retries).to.equal(0, "retries");
+    expect(error instanceof Error).to.equal(true, "error instance");
+    expect(error.message).to.equal("Boom, is aborted");
+    expect(nockScope.isDone()).to.equal(true);
+  });
+});
